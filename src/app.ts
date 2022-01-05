5e04be1871a5455e1e170fcb351734fd7102bd84
Jaume Sancho <jaume.sancho@vehnet.com>
2022-01-05 07:28:28 +0100

    configure prettier

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
-import { program } from "commander";
-import { findMatch } from "./commands/findMatch";
-import { parseDictionaries} from "./commands/parseDictionaries";
+import { program } from 'commander'
+import { findMatch } from './commands/findMatch'
+import { parseDictionaries } from './commands/parseDictionaries'
 
-program.command('suggest')
-        .description('Suggest possible matching words')
-        .action(findMatch);
+program
+  .command('suggest')
+  .description('Suggest possible matching words')
+  .action(findMatch)
 
-program.command('parse-dicts')
-        .description('Load a dictionary and reduce it to 5 characters only')
-        .action(parseDictionaries)
+program
+  .command('parse-dicts')
+  .description('Load a dictionary and reduce it to 5 characters only')
+  .action(parseDictionaries)
 
-program.parse();
+program.parse()
--- src/commands/parseDictionaries.ts
+++ src/commands/parseDictionaries.ts
@@ -1,31 +1,38 @@
-import {promises as fs } from "fs";
-import { Buffer } from "buffer";
-import {EOL} from "os";
+import { promises as fs } from 'fs'
+import { Buffer } from 'buffer'
+import { EOL } from 'os'
 
-const DEFAULT_DICTIONARY = "SCRABBLE-munged-large.txt"
-const WORDLE_DICTIONARY = "wordle.txt"
-const WORD_LENGTH = 5;
+const DEFAULT_DICTIONARY = 'SCRABBLE-munged-large.txt'
+const WORDLE_DICTIONARY = 'wordle.txt'
+const WORD_LENGTH = 5
 
 export const parseDictionaries = async () => {
-
-    try {
+  try {
     // TODO: Parse all available files in "dicts" as a single collection
-    const data = await fs.readFile(`${__dirname}/../dicts/${DEFAULT_DICTIONARY}`,{encoding: "utf8"})
+    const data = await fs.readFile(
+      `${__dirname}/../dicts/${DEFAULT_DICTIONARY}`,
+      { encoding: 'utf8' }
+    )
 
     // TODO: apply multiline regex before splitting the file
-    const words = data.split(/\r?\n/);
+    const words = data.split(/\r?\n/)
     const expression = /^[a-z]{5}$/
-    const wordleDictionary = words.filter(word => expression.test(word))
-                                    .map(word => word.toLowerCase());
+    const wordleDictionary = words
+      .filter((word) => expression.test(word))
+      .map((word) => word.toLowerCase())
 
-    console.log(`${words.length} words have been found`);
-    console.log(`${wordleDictionary.length} with length of ${WORD_LENGTH} have been filtered`);
+    console.log(`${words.length} words have been found`)
+    console.log(
+      `${wordleDictionary.length} with length of ${WORD_LENGTH} have been filtered`
+    )
 
-    const writeBuffer = Buffer.from(wordleDictionary.join(EOL));
-    await fs.writeFile(`${__dirname}/../dicts/${WORDLE_DICTIONARY}`,writeBuffer);
-    }
-    catch (error){
-        console.log("unable to process dictionary files")
-        console.log(error);
-    }
+    const writeBuffer = Buffer.from(wordleDictionary.join(EOL))
+    await fs.writeFile(
+      `${__dirname}/../dicts/${WORDLE_DICTIONARY}`,
+      writeBuffer
+    )
+  } catch (error) {
+    console.log('unable to process dictionary files')
+    console.log(error)
+  }
 }
