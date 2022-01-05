1993d31281e83e8345c73e562ffcc1e78c133073
Jaume Sancho <jaume.sancho@vehnet.com>
2022-01-05 07:19:39 +0100

    add gitignore, remove dist folder

--- dist/commands/findMatch.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.findMatch = void 0;
-const findMatch = () => {
-    console.log("finding matches...");
-};
-exports.findMatch = findMatch;
-//# sourceMappingURL=findMatch.js.map
