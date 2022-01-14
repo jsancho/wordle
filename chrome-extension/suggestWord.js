8fc4b29dd83f1c69c5099c2e489aae4f0c38cfec
Jaume Sancho <jaume.sancho@vehnet.com>
2022-01-14 19:33:28 +0100

    dictionary is loaded during script injection. rather than with every run

--- chrome-extension/suggestWord.js
+++ chrome-extension/suggestWord.js
@@ -1,38 +1,23 @@
+// global variable, so that we can load the dictionary at script injection
+let dictionary = "";
+
 chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
   if (request.action === "suggestWord") {
-    getSuggestions(request.gameResults).then((suggestions) => {
-      // TODO: instead of returning first match, use an strategy,
-      // e.g.
-      // - word that will eliminate the most characters (count of more different characters)
-      // - word that will eliminate the most characters (word more "different" to every other word)
-      // - word with the most commonality (use a popularity ratio based on freedictionary.com, merriamwebster?)
-      const topSuggestion = suggestions[0];
+    const suggestions = getSuggestions(request.gameResults, dictionary);
 
-      // The message port closed before a response was received.
-      sendResponse(topSuggestion);
-    });
+    // TODO: instead of returning first match, use an strategy,
+    // e.g.
+    // - word that will eliminate the most characters (count of more different characters)
+    // - word that will eliminate the most characters (word more "different" to every other word)
+    // - word with the most commonality (use a popularity ratio based on freedictionary.com, merriamwebster?)
+    const topSuggestion = suggestions[0];
+    sendResponse(topSuggestion);
 
     // return true from the event listener to indicate you wish to send a response asynchronously
     // (this will keep the message channel open to the other end until sendResponse is called).
     return true;
   }
 });
-
-const getSuggestions = (gameResults) => {
-  return requestDictionary().then((dictionary) => {
-    return matchResultsToDictionary(gameResults, dictionary);
-  });
-};
-
-const requestDictionary = () => {
-  const file = chrome.runtime.getURL("wordle.txt");
-
-  return fetch(file)
-    .then((response) => response.text())
-    .catch((error) => {
-      console.log(error);
-    });
-};
 
 const filterIncludedLetters = (dictionary, include) => {
   if (!include?.length) return dictionary;
@@ -57,7 +42,7 @@ const filterExcludedLetters = (dictionary, exclude) => {
   return wordTokens.join(",");
 };
 
-const matchResultsToDictionary = (results, dictionary) => {
+const getSuggestions = (results, dictionary) => {
   const { hint, include, exclude, misses } = results;
 
   let workingDictionary = filterIncludedLetters(dictionary, include);
@@ -77,3 +62,17 @@ const matchResultsToDictionary = (results, dictionary) => {
 
   return result.map((r) => r[1]);
 };
+
+const requestDictionary = () => {
+  const file = chrome.runtime.getURL("wordle.txt");
+
+  fetch(file)
+    .then((response) => response.text())
+    .then((text) => {
+      dictionary = text;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+requestDictionary();
